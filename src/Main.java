9fa436ecae9c58cae0cee314faad8adb4b85ce9f
Mei <edygaldy302@gmail.com>
2024-04-22 18:22:29 +0200

    feat(java): added scanner

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        int mortage;
+
+// takes inputs from the terminal
+        System.out.print("Requested amount: ");
+        int requestedAmount = read.nextInt();
+        System.out.print("Interest rate: ");
+        double interestRate = read.nextDouble();
+        System.out.print("Period: ");
+        int period = read.nextInt();
     }
 }
