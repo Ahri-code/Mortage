b1dff98cc26f4e29ee5e7fdd63f5c0f24552590f
Mei <edygaldy302@gmail.com>
2024-04-22 21:55:09 +0200

    feat(java): added do-while loops as checks

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,26 @@ public class Main {
         Scanner read = new Scanner(System.in);
         final byte MONTHS = 12;
         String monthlyPayment;
+        int requestedAmount;
+        double annualInterestRate;
+        int period;
 
-// takes inputs from the terminal
-        System.out.print("Requested amount: ");
-        int requestedAmount = read.nextInt();
-        System.out.print("Interest rate: ");
-// annual percentage calculator
-        double annualInterestRate = read.nextDouble() / 100;
-        double monthlyInterestRate = annualInterestRate / MONTHS;
-        System.out.print("Period: ");
-        int period = read.nextInt();
+// every do-while takes inputs from the terminal checking if the input is an accepted value
+        do {
+            System.out.print("Requested amount (€1k - €1M): ");
+            requestedAmount = read.nextInt();
+        } while(requestedAmount < 1000 || requestedAmount > 1000000);
+
+        do {
+            System.out.print("Interest rate (1 - 30): ");
+            annualInterestRate = read.nextDouble();
+        } while(annualInterestRate < 1 || annualInterestRate > 30);
+        double monthlyInterestRate = annualInterestRate / 100 / MONTHS;
+
+        do {
+            System.out.print("Period (1 - 30): ");
+            period = read.nextInt();
+        } while(period < 1 || period > 30);
 
 // calculate the monthly payment for the mortgage
         double x = Math.pow((1 + monthlyInterestRate), (period * MONTHS));
